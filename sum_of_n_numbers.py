c745fd1672f2cb798abb7e9e2c4c962d2355fd56
Shravanthi <shravanthium111@gmail.com>
2014-06-16 10:41:50 +0530

    Create sum_of_n_numbers.py

---
+++ sum_of_n_numbers.py
@@ -0,0 +1,11 @@
+#find the sum of n numbers
+num=int(input("Enter a number: "))
+sum=0
+if(num==0):
+    print("Sum is 0")
+elif(num==None):
+    print("Please enter something")
+else:
+    for i in range(1,num+1):
+        sum=sum+i
+    print(sum)
