fd19e449465ea10be7bb4b260838b7fbf87cdf3f
Shravanthi <shravanthium111@gmail.com>
2014-06-16 14:12:37 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,1 +1,17 @@
 #permutation of a number
+
+def perm(l):
+        # Compute the list of all permutations of l
+    if len(l) <= 1:
+                  return [l]
+    r = []  # here is new list with all permutations!
+    for i in range(len(l)):
+             s = l[:i] + l[i+1:]
+             p = perm(s)
+             for x in p:
+              r.append(l[i:i+1] + x)
+    return r
+
+a=input("Enter a Set:")
+
+print perm(a)
